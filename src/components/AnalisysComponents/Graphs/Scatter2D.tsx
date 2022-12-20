9cb409f1c4f72a6c77cf2fb949a0c13dc6d6fe6c
mino_mac <minoruaka@gmail.com>
2022-12-21 00:11:40 +0900

    update(Scatter2D.tsx): レスポンシブ化, マージン調整

--- src/components/AnalisysComponents/Graphs/Scatter2D.tsx
+++ src/components/AnalisysComponents/Graphs/Scatter2D.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
-import { useEventContext } from '../../state/EventProvider';
 import { CategoryInfo, EventLog } from '../../utilTypes';
+import * as Plotly from 'plotly.js';
 
 enum Score {
   Socially,
@@ -9,28 +9,22 @@ enum Score {
   Lecture,
 }
 
-interface ScatterGraphParams {
-  x: number[];
-  y: number[];
-  mode: string;
-  text: string[];
-  xaxis: '個性的-社会的' | '実用性-教養性';
-  yaxis: '教養性-実用性' | '参加型-講義型';
-}
-const scatterScores = (eventLogs: EventLog[], allCategories: CategoryInfo[], type: 1 | 2 | 3): ScatterGraphParams => {
+const scatterScores = (eventLogs: EventLog[], allCategories: CategoryInfo[], type: 1 | 2 | 3): Plotly.Data[] => {
   // X軸は社会的/教養的の2種類。Y軸は教養的/講義型の2種で組み合わせているのでこの書き方でOK
   const xScores = type === 2 ? calcScore(eventLogs, allCategories, Score.Educated) : calcScore(eventLogs, allCategories, Score.Socially);
   const yScores = type === 1 ? calcScore(eventLogs, allCategories, Score.Educated) : calcScore(eventLogs, allCategories, Score.Lecture);
   const xAxis = type === 2 ? '実用性-教養性' : '個性的-社会的';
   const yAxis = type === 1 ? '教養性-実用性' : '参加型-講義型';
-  return {
-    x: xScores,
-    y: yScores,
-    mode: 'markers',
-    text: eventLogs.map((v) => v.title),
-    xaxis: xAxis,
-    yaxis: yAxis,
-  };
+  return [
+    {
+      x: xScores,
+      y: yScores,
+      mode: 'markers',
+      text: eventLogs.map((v) => v.title),
+      xaxis: xAxis,
+      yaxis: yAxis,
+    },
+  ];
 };
 
 const calcScore = (eventLogs: EventLog[], allCategories: CategoryInfo[], scoreKind: Score): number[] => {
@@ -56,10 +50,24 @@ const titleLabel = (type: 1 | 2 | 3): string => {
 
 interface props {
   type: 1 | 2 | 3;
+  eventLogs: EventLog[];
+  categories: CategoryInfo[];
 }
-const Scatter2D: React.FC<props> = ({ type }) => {
-  const { allEventLogs, allCategory } = useEventContext();
-  return <Plot data={[scatterScores(allEventLogs, allCategory, type)]} layout={{ title: titleLabel(type) }} />;
+const Scatter2D: React.FC<props> = ({ type, eventLogs, categories }) => {
+  return (
+    <Plot
+      data={scatterScores(eventLogs, categories, type)}
+      layout={{
+        title: titleLabel(type),
+        autosize: true,
+        xaxis: { range: [-10, 10] },
+        yaxis: { range: [-10, 10] },
+        margin: { t: 50, b: 40, l: 40, r: 40 },
+      }}
+      useResizeHandler={true}
+      style={{ width: '100%', height: '100%' }}
+    />
+  );
 };
 
 export default Scatter2D;
