53883aa1e79d43172126bb0aa60893464538b354
Mino996996 <minoruaka@gmail.com>
2023-05-08 03:27:48 +0900

    refactor: コンポーネントの分離

--- src/components/Config/Config.tsx
+++ src/components/Config/Config.tsx
@@ -1,20 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { pickRelationalTabs } from '../EventComponents/cardFunctions';
+import React from 'react';
 import { ConfigToggleButton } from './ConfigToggleButton';
-import { useConfigContext } from '../state/ConfigProvider';
-import { ConfigSortToggleButton } from './ConfigSortToggleButton';
 import { useEventContext } from '../state/EventProvider';
+import ConfigTags from './ConfigTags';
 
 const Config: React.FC = () => {
-  const { keywords, setKeywords, selectedCategory } = useConfigContext();
   const { allEventLogs, fetchEventsAndCategories } = useEventContext();
-  const [itemList, setItemList] = useState<string[]>([]);
-
-  useEffect(() => {
-    const items = pickRelationalTabs(allEventLogs, selectedCategory);
-    setItemList([...items]);
-  }, [selectedCategory, allEventLogs]);
-
   return (
     <>
       <div className="pt-2 sm:pt-6 text-gray-200 text-lg font-bold text-center">
@@ -26,7 +16,7 @@ const Config: React.FC = () => {
           コメント：省略 <ConfigToggleButton kind={'memo'} /> 全文
         </p>
         <p>
-          表示順：OLD <ConfigSortToggleButton /> NEW
+          表示順：OLD <ConfigToggleButton kind={'asc'} /> NEW
         </p>
       </div>
       <div className="pt-2 sm:pt-6 text-lg font-bold text-center">
@@ -42,36 +32,7 @@ const Config: React.FC = () => {
           Reload
         </button>
       </div>
-      <div className="pt-2 sm:pt-6 text-lg">
-        <h2 className="text-center mb-2 font-bold text-red-300">= {selectedCategory !== '' && selectedCategory + ' 関連'}タグ一覧 =</h2>
-        <div className="overflow-y-scroll edit-scrollbar" style={{ height: '52vh' }}>
-          {itemList
-            .filter((value) => value !== selectedCategory)
-            .map((item) => (
-              <div className="inline-block" key={item}>
-                <input
-                  type="checkbox"
-                  id={item}
-                  className="cursor-pointer"
-                  checked={keywords.includes(item)}
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    if (e.target.checked) {
-                      setKeywords([...keywords, item]);
-                      localStorage.setItem('keyword', JSON.stringify([...keywords, item]));
-                    } else {
-                      const newList = keywords.filter((value) => value !== item);
-                      setKeywords([...newList]);
-                      localStorage.setItem('keyword', JSON.stringify([...newList]));
-                    }
-                  }}
-                />
-                <label htmlFor={item} className="pl-2 mr-6 text-base cursor-pointer text-gray-200">
-                  {item}
-                </label>
-              </div>
-            ))}
-        </div>
-      </div>
+      <ConfigTags allEventLogs={allEventLogs} />
     </>
   );
 };
